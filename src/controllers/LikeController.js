d8a92439f8258257a9a003c98ca13705d6a3caa9
Adonis Amarante <adonisamarante@gmail.com>
2019-08-11 13:24:42 -0300

    aula backend finalizada

--- src/controllers/LikeController.js
+++ src/controllers/LikeController.js
@@ -2,8 +2,13 @@ const Post = require("../models/Post");
 
 module.exports = {
   async store(req, res) {
-    return res.json({ ok: true });
+    const post = await Post.findById(req.params.id);
+
+    post.likes += 1;
+    await post.save();
+
+    req.io.emit("like", post);
+
+    return res.json(post);
   }
 };
-
-//---------------- 41 minutos ----------------
--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require("path");
+const cors = require("cors");
 
 //cria um servidor
 const app = express();
+
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+
 mongoose.connect(
   "mongodb+srv://adonis:adonis@cluster0-lmtvc.mongodb.net/test?retryWrites=true&w=majority",
   {
@@ -10,6 +16,19 @@ mongoose.connect(
   }
 );
 
+app.use((req, res, next) => {
+  req.io = io;
+
+  next(); //next garante a execução do resto do código
+});
+
+app.use(cors());
+
+app.use(
+  "/files",
+  express.static(path.resolve(__dirname, "..", "uploads", "resized"))
+);
+
 app.use(require("./routes"));
 
-app.listen(3333);
+server.listen(3333);
