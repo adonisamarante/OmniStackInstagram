a2c983ca5ac6cfc0c1d92b2501292fa14dfbb0ad
Adonis Amarante <adonisamarante@gmail.com>
2019-08-10 23:15:40 -0300

    continuacao do curso

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,15 @@
 const express = require("express");
-const routes = new express.Router();
+const multer = require("multer");
+const uploadConfig = require("./config/upload");
 
-routes.get("/", (req, res) => {
-  return res.send(`Hello ${req.query.name}`);
-});
+const PostController = require("./controllers/PostController");
+const LikeController = require("./controllers/LikeController");
+
+const routes = new express.Router();
+const upload = multer(uploadConfig);
+
+routes.get("/posts", PostController.index);
+routes.post("/posts", upload.single("image"), PostController.store);
+routes.post("/posts/:id/like", LikeController.store);
 
 module.exports = routes;
